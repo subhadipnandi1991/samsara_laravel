7344158a0ba174ecc30cac712565cc2f019c6c0b
subhadipnandi1991 <subha.nandi2014@gmail.com>
2021-10-17 20:44:53 +0530

    Login page, User table, accessPage created. Header, footer inserted. sample data has been inserted.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,13 +1,27 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Samsara</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  </head>
-  <body>
-    Header
-  </body>
-</html>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse w-50" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Solutions</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Customers</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Resources</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Company</a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,26 @@
-@extends('header')
+@extends('accessPage')
+
 @section('content')
-<h1>Login Page</h1>
-endsection
-@extends('footer')
+<div class="row">
+  <div class="col-4 p-5" id="MyLoginContainer">
+    <form>
+      <h2 class="m-3 text-center">Sign In</h2>
+      <div class="form-group">
+        <input type="email" class="form-control m-3" id="email" aria-describedby="emailHelp" placeholder="name@work-email.com">
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control m-3" id="password" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-primary mx-3 w-100">Sign In</button>
+      <div class="text-center row m-3 px-5">
+        <a href="#" class="navbar-link col-6 text-black">Sign up</a>
+        <a href="#" class="navbar-link col-6 text-black">Reset Password</a>
+      </div>
+
+    </form>
+  </div>
+  <div class="col-8" id="MyLoginBackground">
+
+  </div>
+</div>
+@endsection
