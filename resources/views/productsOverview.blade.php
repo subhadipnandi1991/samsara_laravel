a33158400e30c44221e16c6b20a51f4340a6e2fc
subhadipnandi1991 <subha.nandi2014@gmail.com>
2021-10-25 21:48:28 +0530

    contents fixed

--- resources/views/productsOverview.blade.php
+++ resources/views/productsOverview.blade.php
@@ -1,8 +1,8 @@
 @extends('master')
 
-@section
+@section('content')
 <div>
   <h1>Products Overview</h1>
   <p>Samsara’s integrated platform to increase safety, efficiency, and sustainability </p>
 </div>
-endsection
+@endsection
